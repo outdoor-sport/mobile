149258d94975ce2a4a5a6f9cf5d52a439161db5a
Tom <tomlaplace0902@gmail.com>
2024-12-23 19:34:24 +0100

    pass service to class

--- src/domain/services/authenticate_user/authenticate_user.ts
+++ src/domain/services/authenticate_user/authenticate_user.ts
@@ -1,8 +1,5 @@
 import { User } from "@/domain/models/User";
 
-export const authenticateUser = () => ({
-  authenticateThroughApi: (user: User) => Promise<User>,
-  authenticateThroughLocalStorage: (user: User) => Promise<User>,
-  authenticateThroughApple: (user: User) => Promise<User>,
-  authenticateThroughGoogle: (user: User) => Promise<User>,
-});
+export class AuthenticateUser {}
+
+export default AuthenticateUser;
