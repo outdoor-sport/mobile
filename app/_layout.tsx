6759c736f376511e529afa3f5e118ccfea19735a
Tom <tomlaplace0902@gmail.com>
2024-12-24 09:32:23 +0100

    setup fresh app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,36 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+import { useColorScheme } from '@react-native/hooks/useColorScheme';
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout(){
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if(loaded){
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if(!loaded){
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  )
 }
