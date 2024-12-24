fa88abdc05fcdba88ef84b91895d129b96ec0b4e
Tom <tomlaplace0902@gmail.com>
2024-12-24 10:18:00 +0100

    wip

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { AppleSignInButton } from "@/src/infrastructure/react-native/components/auth/AppleSignInButton";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +11,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <AppleSignInButton />
     </View>
   );
 }
